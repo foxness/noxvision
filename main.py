0c433e9c5f804d218c3c0e2aa2a8dbe0bc11f1aa
nocturnal <foxnezz@gmail.com>
2020-05-14 10:32:31 +0300

    added object tracking

--- main.py
+++ main.py
@@ -1,107 +1,140 @@
 # USAGE
-# python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+# python opencv_object_tracking.py
+# python opencv_object_tracking.py --video dashcam_boston.mp4 --tracker csrt
 
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
-import numpy as np
 import argparse
 import imutils
 import time
 import cv2
 
-# construct the argument parse and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-p", "--prototxt", required=True,
-# 	help="path to Caffe 'deploy' prototxt file")
-# ap.add_argument("-m", "--model", required=True,
-# 	help="path to Caffe pre-trained model")
-# ap.add_argument("-c", "--confidence", type=float, default=0.2,
-# 	help="minimum probability to filter weak detections")
-# args = vars(ap.parse_args())
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", type=str, default="R:\\my\\drive\\sync\\things\\projects\\noxvisioncloud\\opencv-object-tracking\\american_pharoah.mp4",
+	help="path to input video file")
+ap.add_argument("-t", "--tracker", type=str, default="csrt",
+	help="OpenCV object tracker type")
+args = vars(ap.parse_args())
 
-args = {'prototxt': 'MobileNetSSD_deploy.prototxt.txt',
-        'model': 'MobileNetSSD_deploy.caffemodel',
-        'confidence': 0.2,
-        'videofile': 'R:\\my\\drive\\sync\\things\\projects\\noxvisioncloud\\Can You Win Blindfolded Musical Chairs_.mp4'}
+# extract the OpenCV version info
+(major, minor) = cv2.__version__.split(".")[:2]
 
-# initialize the list of class labels MobileNet SSD was trained to
-# detect, then generate a set of bounding box colors for each class
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
-COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+# if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
+# function to create our object tracker
+if int(major) == 3 and int(minor) < 3:
+	tracker = cv2.Tracker_create(args["tracker"].upper())
 
-# load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+# otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
+# approrpiate object tracker constructor:
+else:
+	# initialize a dictionary that maps strings to their corresponding
+	# OpenCV object tracker implementations
+	OPENCV_OBJECT_TRACKERS = {
+		"csrt": cv2.TrackerCSRT_create,
+		"kcf": cv2.TrackerKCF_create,
+		"boosting": cv2.TrackerBoosting_create,
+		"mil": cv2.TrackerMIL_create,
+		"tld": cv2.TrackerTLD_create,
+		"medianflow": cv2.TrackerMedianFlow_create,
+		"mosse": cv2.TrackerMOSSE_create
+	}
 
-# initialize the video stream, allow the cammera sensor to warmup,
-# and initialize the FPS counter
-print("[INFO] starting video stream...")
-vs = VideoStream(args["videofile"]).start()
-time.sleep(2.0)
-fps = FPS().start()
+	# grab the appropriate object tracker using our dictionary of
+	# OpenCV object tracker objects
+	tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
 
-# loop over the frames from the video stream
+# initialize the bounding box coordinates of the object we are going
+# to track
+initBB = None
+
+# if a video path was not supplied, grab the reference to the web cam
+if not args.get("video", False):
+	print("[INFO] starting video stream...")
+	vs = VideoStream(src=0).start()
+	time.sleep(1.0)
+
+# otherwise, grab a reference to the video file
+else:
+	vs = cv2.VideoCapture(args["video"])
+
+# initialize the FPS throughput estimator
+fps = None
+
+# loop over frames from the video stream
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
+	# grab the current frame, then handle if we are using a
+	# VideoStream or VideoCapture object
 	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+	frame = frame[1] if args.get("video", False) else frame
 
-	# grab the frame dimensions and convert it to a blob
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-		0.007843, (300, 300), 127.5)
+	# check to see if we have reached the end of the stream
+	if frame is None:
+		break
 
-	# pass the blob through the network and obtain the detections and
-	# predictions
-	net.setInput(blob)
-	detections = net.forward()
+	# resize the frame (so we can process it faster) and grab the
+	# frame dimensions
+	frame = imutils.resize(frame, width=500)
+	(H, W) = frame.shape[:2]
 
-	# loop over the detections
-	for i in np.arange(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the prediction
-		confidence = detections[0, 0, i, 2]
+	# check to see if we are currently tracking an object
+	if initBB is not None:
+		# grab the new bounding box coordinates of the object
+		(success, box) = tracker.update(frame)
 
-		# filter out weak detections by ensuring the `confidence` is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# extract the index of the class label from the
-			# `detections`, then compute the (x, y)-coordinates of
-			# the bounding box for the object
-			idx = int(detections[0, 0, i, 1])
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+		# check to see if the tracking was a success
+		if success:
+			(x, y, w, h) = [int(v) for v in box]
+			cv2.rectangle(frame, (x, y), (x + w, y + h),
+				(0, 255, 0), 2)
 
-			# draw the prediction on the frame
-			label = "{}: {:.2f}%".format(CLASSES[idx],
-				confidence * 100)
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				COLORS[idx], 2)
-			y = startY - 15 if startY - 15 > 15 else startY + 15
-			cv2.putText(frame, label, (startX, y),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+		# update the FPS counter
+		fps.update()
+		fps.stop()
+
+		# initialize the set of information we'll be displaying on
+		# the frame
+		info = [
+			("Tracker", args["tracker"]),
+			("Success", "Yes" if success else "No"),
+			("FPS", "{:.2f}".format(fps.fps())),
+		]
+
+		# loop over the info tuples and draw them on our frame
+		for (i, (k, v)) in enumerate(info):
+			text = "{}: {}".format(k, v)
+			cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
 	# show the output frame
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
+	# if the 's' key is selected, we are going to "select" a bounding
+	# box to track
+	if key == ord("s"):
+		# select the bounding box of the object we want to track (make
+		# sure you press ENTER or SPACE after selecting the ROI)
+		initBB = cv2.selectROI("Frame", frame, fromCenter=False,
+			showCrosshair=True)
+
+		# start OpenCV object tracker using the supplied bounding box
+		# coordinates, then start the FPS throughput estimator as well
+		tracker.init(frame, initBB)
+		fps = FPS().start()
+
 	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
+	elif key == ord("q"):
 		break
 
-	# update the FPS counter
-	fps.update()
+# if we are using a webcam, release the pointer
+if not args.get("video", False):
+	vs.stop()
 
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+# otherwise, release the file pointer
+else:
+	vs.release()
 
-# do a bit of cleanup
+# close all windows
 cv2.destroyAllWindows()
-vs.stop()
