7446da8fc86fe5fd9dcbdf66d0a0b07b36a81e35
nocturnal <foxnezz@gmail.com>
2020-05-15 09:20:36 +0300

    removed centroid tracker

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from pyimagesearch.centroidtracker import CentroidTracker
-from pyimagesearch.trackableobject import TrackableObject
-from imutils.video import VideoStream
 from imutils.video import FPS
 import numpy as np
 import argparse
 import imutils
-import time
 import dlib
 import cv2
 
@@ -43,163 +39,94 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 
 # ---------------------------------------------------------------------------
 
-def draw_rect(frame, startX, startY, endX, endY):
+def draw_box(frame, startX, startY, endX, endY):
     cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+
+def draw_box_text(frame, startX, startY, endX, endY, text):
+    draw_box(frame, startX, startY, endX, endY)
+    cv2.putText(frame, text, (startX, startY - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
 
 def main():
     print("[INFO] loading model...")
     net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
 
     print("[INFO] opening video file...")
-    vs = cv2.VideoCapture(input_video)
+    video = cv2.VideoCapture(input_video)
 
     writer = None
-    analysis = { 'frames': [] }
-
+    trackers = []
+    labels = []
     W = None
     H = None
-
-    # instantiate our centroid tracker, then initialize a list to store
-    # each of our dlib correlation trackers, followed by a dictionary to
-    # map each unique object ID to a TrackableObject
-    ct = CentroidTracker(maxDisappeared = 40, maxDistance = 50)
-    trackers = []
-    trackableObjects = {}
-
-    frame_count = int(vs.get(cv2.CAP_PROP_FRAME_COUNT))
+    analysis = { 'frames': [] }
     frames_processed = 0
+    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = FPS().start()
+    rects = []
 
     while True:
-        frame = vs.read()
-        frame = frame[1]
+        rects.clear()
+        (grabbed, frame) = video.read()
 
         if frame is None:
             # end of video
             break
 
-        # resize the frame to have a maximum width of 500 pixels (the
-        # less data we have, the faster we can process it), then convert
-        # the frame from BGR to RGB for dlib
-        frame = imutils.resize(frame, width = 500)
+        frame = imutils.resize(frame, width = 600)
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         if W is None or H is None:
             (H, W) = frame.shape[:2]
 
-        # if we are supposed to be writing a video to disk, initialize
-        # the writer
         if export_video is not None and writer is None:
             fourcc = cv2.VideoWriter_fourcc(*"MJPG")
             writer = cv2.VideoWriter(export_video, fourcc, 30, (W, H), True)
 
-        # initialize the current status along with our list of bounding
-        # box rectangles returned by either (1) our object detector or
-        # (2) the correlation trackers
-        status = "Waiting"
-        rects = []
-
-        # check to see if we should run a more computationally expensive
-        # object detection method to aid our tracker
         if frames_processed % skip_frames == 0:
             print("frame {}/{} ({:.0%})".format(frames_processed, frame_count, frames_processed / frame_count))
-            # set the status and initialize our new set of object trackers
-            status = "Detecting"
-            trackers = []
 
-            # convert the frame to a blob and pass the blob through the
-            # network and obtain the detections
+        if len(trackers) == 0:
+            status = "Detecting"
+
             blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
             net.setInput(blob)
             detections = net.forward()
 
-            # loop over the detections
             for i in np.arange(0, detections.shape[2]):
-                # extract the confidence (i.e., probability) associated
-                # with the prediction
                 confidence = detections[0, 0, i, 2]
 
-                # filter out weak detections by requiring a minimum
-                # confidence
                 if confidence > confidence_threshold:
-                    # extract the index of the class label from the
-                    # detections list
                     idx = int(detections[0, 0, i, 1])
+                    label = CLASSES[idx]
 
-                    # if the class label is not a person, ignore it
-                    if CLASSES[idx] != "person":
+                    if label != "person":
                         continue
 
-                    # compute the (x, y)-coordinates of the bounding box
-                    # for the object
                     box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
                     (startX, startY, endX, endY) = box.astype("int")
-                    draw_rect(frame, startX, startY, endX, endY)
 
-                    # construct a dlib rectangle object from the bounding
-                    # box coordinates and then start the dlib correlation
-                    # tracker
                     tracker = dlib.correlation_tracker()
                     rect = dlib.rectangle(startX, startY, endX, endY)
                     tracker.start_track(rgb, rect)
 
-                    # add the tracker to our list of trackers so we can
-                    # utilize it during skip frames
+                    labels.append(label)
                     trackers.append(tracker)
 
-        # otherwise, we should utilize our object *trackers* rather than
-        # object *detectors* to obtain a higher frame processing throughput
+                    draw_box_text(frame, startX, startY, endX, endY, label)
         else:
-            # loop over the trackers
-            for tracker in trackers:
-                # set the status of our system to be 'tracking' rather
-                # than 'waiting' or 'detecting'
+            for (t, l) in zip(trackers, labels):
                 status = "Tracking"
 
-                # update the tracker and grab the updated position
-                tracker.update(rgb)
-                pos = tracker.get_position()
+                t.update(rgb)
+                pos = t.get_position()
 
-                # unpack the position object
                 startX = int(pos.left())
                 startY = int(pos.top())
                 endX = int(pos.right())
                 endY = int(pos.bottom())
 
-                # add the bounding box coordinates to the rectangles list
                 rects.append((startX, startY, endX, endY))
-                draw_rect(frame, startX, startY, endX, endY)
-
-        # use the centroid tracker to associate the (1) old object
-        # centroids with (2) the newly computed object centroids
-        objects = ct.update(rects)
-
-        # loop over the tracked objects
-        for (objectID, centroid) in objects.items():
-            # check to see if a trackable object exists for the current
-            # object ID
-            to = trackableObjects.get(objectID, None)
-
-            # if there is no existing trackable object, create one
-            if to is None:
-                to = TrackableObject(objectID, centroid)
-
-            # otherwise, there is a trackable object so we can utilize it
-            # to determine direction
-            else:
-                y = [c[1] for c in to.centroids]
-                direction = centroid[1] - np.mean(y)
-                to.centroids.append(centroid)
-
-            # store the trackable object in our dictionary
-            trackableObjects[objectID] = to
-
-            # draw both the ID of the object and the centroid of the
-            # object on the output frame
-            text = "ID {}".format(objectID)
-            cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+                draw_box_text(frame, startX, startY, endX, endY, l)
 
         info = [
             ("Status", status)
@@ -215,8 +142,8 @@ def main():
 
         if draw:
             cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
 
+        key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
 
@@ -230,7 +157,7 @@ def main():
     if writer is not None:
         writer.release()
 
-    vs.release()
+    video.release()
 
     if draw:
         cv2.destroyAllWindows()
