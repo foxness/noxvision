331267c17455602f80ac5656cf3630e89d8b6ba3
nocturnal <foxnezz@gmail.com>
2020-05-19 22:53:10 +0300

    added drawing the pie chart

--- core_app/NoxVision/ReportGenerator.cs
+++ core_app/NoxVision/ReportGenerator.cs
@@ -19,6 +19,7 @@ namespace NoxVision
 
         private Brush backgroundBrush;
         private Brush textBrush;
+        private List<Brush> niceBrushes;
 
         public ReportGenerator(string videoFilepath, AnalysisInfo analysisInfo)
         {
@@ -29,6 +30,17 @@ namespace NoxVision
 
             backgroundBrush = new SolidBrush(Color.FromArgb(17, 17, 17));
             textBrush = new SolidBrush(Color.FromArgb(255, 255, 255));
+
+            niceBrushes = new List<Brush>();
+            niceBrushes.Add(new SolidBrush(Color.FromArgb(14, 105, 16)));
+            niceBrushes.Add(new SolidBrush(Color.FromArgb(255, 67, 0)));
+            niceBrushes.Add(new SolidBrush(Color.FromArgb(101, 21, 127)));
+            niceBrushes.Add(new SolidBrush(Color.FromArgb(255, 189, 0)));
+            niceBrushes.Add(new SolidBrush(Color.FromArgb(255, 0, 81)));
+            niceBrushes.Add(new SolidBrush(Color.FromArgb(143, 155, 249)));
+            niceBrushes.Add(new SolidBrush(Color.FromArgb(191, 118, 210)));
+            niceBrushes.Add(new SolidBrush(Color.FromArgb(194, 75, 79)));
+            niceBrushes.Add(new SolidBrush(Color.FromArgb(102, 104, 191)));
         }
 
         public Bitmap GenerateReport()
@@ -79,10 +91,30 @@ namespace NoxVision
             return output;
         }
 
+        private void DrawSector(Graphics g, Brush b, int x, int y, int radius, float percentage)
+        {
+            g.FillPie(b, x - radius, y - radius, radius * 2, radius * 2, -90, percentage * 360);
+        }
+
         private void DrawStatCircle(Graphics g)
         {
+            int x = 500;
+            int y = 650;
+            int r = 230;
+
             var stats = GetStatCircle();
-            var a = 1;
+            DrawSector(g, niceBrushes[0], x, y, r, 1f);
+
+            var categories = stats.Keys.OrderBy(k => stats[k]);
+            int i = 1;
+            float buildup = 0;
+            foreach (var category in categories)
+            {
+                var percentage = (float)stats[category];
+                DrawSector(g, niceBrushes[i], x, y, r, 1 - (percentage + buildup));
+                buildup += percentage;
+                i++;
+            }
         }
 
         private void DrawHeader(Graphics g)
